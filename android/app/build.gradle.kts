b380f9609ad678ff757b65f4a8f8ff31a4046bdf
Fanboynz <mp3geek@gmail.com>
2026-08-16 02:08:52 +1200

    chore(staging): bump versionCode 336->337 for the refreshed 10.1.0 testing build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.noop.whoop"
         minSdk = 26
         targetSdk = 34
-        versionCode = 336
+        versionCode = 337
         versionName = "10.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
